9adb19a8297528d850433be885912813848c11b5
= <=>
2023-06-10 14:46:43 +0100

    Add multi delegates implementation

--- DelegatePractice/Program.cs
+++ DelegatePractice/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using DelegatePractice;
+
+TestDelegate.UseDelegateFunction();
+
+MultiDelegate m = Method1;
+m += Method2;
+m();
+
+Console.ReadKey();
+
+static void Method1()
+{
+    Console.WriteLine("Invoked method 1");
+}
+
+static void Method2()
+{
+    Console.WriteLine("Invoked method 2");
+}
+public delegate void MultiDelegate();
--- DelegatePractice/TestDelegate.cs
+++ DelegatePractice/TestDelegate.cs
@@ -1,6 +1,30 @@
 namespace DelegatePractice;
 
-public class TestDelegate
+public static class TestDelegate
 {
-    
+    public delegate int SimpleDelegate(string s);
+
+    public delegate void EventHandler(string value);
+
+    private static int StringLength(string s)
+    {
+        return s.Length;
+    }
+
+    public static event EventHandler ErrorOccured;
+
+    public static void UseDelegateFunction()
+    {
+        SimpleDelegate d = StringLength;
+        int length = d.Invoke("Hello World");
+        Console.WriteLine("Result is: " + length);
+
+        ErrorOccured += SomethingHappened;
+        ErrorOccured("Exception in main thread");
+    }
+
+    public static void SomethingHappened(string value)
+    {
+        Console.WriteLine(value);
+    }
 }
